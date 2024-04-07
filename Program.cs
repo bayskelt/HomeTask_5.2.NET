c3ff5cfa495b450710465e44ee5234c64e7507c1
bayskelt <rinognom@gmail.com>
2024-04-08 00:17:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace HomeTask_5._2.NET
+{
+    internal class Program
+    {
+        static int Maximum(int[] mas)
+        {
+            int max = mas[0];
+            foreach (int i in mas)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть кількість елементів списку: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] mas = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Введіть {i + 1} елемент списку: ");
+                mas[i] = int.Parse(Console.ReadLine());
+            }
+            int maxi = Maximum(mas);
+            Console.WriteLine($"Максимальний елемент: {maxi}");
+        }
+    }
+}
+
